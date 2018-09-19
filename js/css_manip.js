eb65c9dc07449bd47272b82cbad230f7a22c245d
afleischer <afleischer1337@gmail.com>
2018-09-18 17:57:55 -0700

    Added footer, added responsive background image offsets

---
+++ js/css_manip.js
@@ -0,0 +1,9 @@
+
+
+var first_svg = document.querySelector('.div-block-17');
+
+
+
+function changeSVGHeight(){
+
+}
--- js/svg_manip.js
+++ js/svg_manip.js
@@ -1,2 +1,4 @@
 import SVG from './svg.js';
 
+var draw = SVG('svgTest').size(100, 100)
+var drawing = draw.
